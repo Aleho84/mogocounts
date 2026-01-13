8a09ab98bb1fb8e13f4ac93742ae5b7ef38eac24
Alejandro Abraham <alejandro.r.abraham@gmail.com>
2026-01-13 01:48:26 -0300

    [MODIFY] [NottonNav.jsx] -Se modifica la apariencia del boton de agregar gasto (+)

--- client/src/components/BottomNav.jsx
+++ client/src/components/BottomNav.jsx
@@ -4,16 +4,16 @@ import { Receipt, Scale, Settings, Plus } from 'lucide-react';
 import { useStore } from '../store/useStore';
 
 const BottomNav = () => {
-    const { currentGroup } = useStore();
+    const { currentGroup, error } = useStore();
     const location = useLocation();
 
-    if (location.pathname === '/') return null;
+    if (location.pathname === '/' || error) return null;
 
     if (!currentGroup) return (
         <div className="fixed bottom-6 left-1/2 -translate-x-1/2 z-50 w-full max-w-sm px-6">
             <div className="flex justify-between items-center bg-slate-900/80 backdrop-blur-xl border border-slate-700/50 rounded-3xl p-2 shadow-2xl shadow-black/50 h-[66px] animate-pulse">
                 <div className="w-12 h-12 rounded-2xl bg-slate-800/50 mx-1"></div>
-                <div className="w-14 h-14 rounded-full bg-slate-800/50 -mt-8 border-4 border-slate-900"></div>
+                <div className="w-18 h-18 rounded-full bg-slate-800/50 -mt-8 border-4 border-slate-900"></div>
                 <div className="w-12 h-12 rounded-2xl bg-slate-800/50 mx-1"></div>
                 <div className="w-12 h-12 rounded-2xl bg-slate-800/50 mx-1"></div>
             </div>
@@ -41,7 +41,10 @@ const BottomNav = () => {
 
                 <NavLink
                     to={`/group/${currentGroup._id}/add`}
-                    className="flex items-center justify-center w-14 h-14 bg-indigo-500 rounded-full text-white shadow-lg shadow-indigo-500/40 transition-transform active:scale-95 hover:scale-105"
+                    className={({ isActive }) => `
+                        flex items-center justify-center w-14 h-14 bg-indigo-500 rounded-full text-white shadow-lg shadow-indigo-500/40 transition-all duration-300
+                        ${isActive ? 'scale-125 ring-4 ring-slate-900' : 'active:scale-95 hover:scale-110'}
+                    `}
                 >
                     <Plus size={32} />
                 </NavLink>
